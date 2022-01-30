288e6750d0f0e9ba9c1ebb0992bb434f506f6ad2
gutwenger <chengfremont@gmail.com>
2022-01-30 14:03:55 +0800

    Added raw endpoint

--- src/index.ts
+++ src/index.ts
@@ -49,6 +49,24 @@ app.get('/get-schedule', (req: Request, res: Response) => {
   res.send(returnHTML);
 })
 
+app.get('/get-schedule-raw', (req: Request, res: Response) => {
+  const query = req.query;
+
+  const numberOfCandidates = new BigNumber(`${query.numberOfCandidates}`);
+
+  if (!numberOfCandidates || numberOfCandidates.isNaN()) {
+    res.send('MUST HAVE NUMBER OF CANDIDATES QUERY');
+  }
+
+  if (numberOfCandidates.toNumber() % 2 !== 0) {
+    res.send('NUMBER OF CANDIDATES MUST BE EVEN');
+  }
+
+  const result = getSchedule(numberOfCandidates.toNumber());
+
+  res.send(result);
+})
+
 app.listen(port, () => {
   return console.log(`@Server is listening on port: ${port}`);
 })
